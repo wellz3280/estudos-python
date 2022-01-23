0e0927ae31e4132c4c164b15b9c4ad554abd0843
weliton <welingtonzsilva@gmail.com>
2022-01-23 00:53:24 -0300

    estudos pythos

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,17 +1,41 @@
+import random
+
 print("*********************************")
 print("bem vindo ao jogo de adivinhação")
 print("*********************************")
 
-numero_secreto = 42
 # A funcão input sempre devolve uma string
-chute = input("digite o seu numero: ")
-
 # funcão int() converte string para inteiro
-nunero_digitado = int(chute)
+# gerando numero aleatorio com a biblioteca random
 
-if(numero_secreto == nunero_digitado):
-    print("voce acertou")
-else:
-    print("voce errou")
+numero_secreto = round(random.randrange(1,101))
+total_de_tentativas = 3
+rodada = 1
+print(numero_secreto)
+for rodada in range(1,total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada,total_de_tentativas))
+    chute = input("digite o seu numero: ")
 
-print("fim do jogo")
+    if(int(chute) < 1 or int(chute) > 100):
+        print("digite um numero entre 1 e 100")
+        continue
+
+    nunero_digitado = int(chute)
+    print("********************************")
+
+    acertou=nunero_digitado == numero_secreto
+    maior = nunero_digitado > numero_secreto
+    menor = nunero_digitado < numero_secreto
+
+    if(acertou):
+        print("voce acertou")
+        break
+    else:
+        if(maior):
+            print("voce errou seu chute foi maior que o numero secreto")
+        elif(menor):
+            print("voce errou seu chute foi menor que o numero secreto")
+    print("********************************")
+
+if(rodada == 3):
+    print("Game Over")
---
+++ codigo_barras.py
@@ -0,0 +1,14 @@
+print("**********Scaneie o produto*********")
+
+
+produto = "ameixa seca"
+cod_ameixa = 123456789
+valor = 12.95
+
+escaneado_str = input("Escaneie o Produto: ")
+escaneado_int = int(escaneado_str)
+
+if(escaneado_int == cod_ameixa):
+    print("Produto {} R$ {:.2f}".format(produto,valor))
+else:
+    print("Tente Novamente Por favor")
